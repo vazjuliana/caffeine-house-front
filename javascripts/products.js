9a7a7821c9dd68db46710a0846dc333af509dbe1
Juliana Vaz da Silva <vazjuliana1993@gmail.com>
2022-05-12 20:29:42 -0300

    pagina inicial

--- javascripts/products.js
+++ javascripts/products.js
@@ -9,10 +9,10 @@ axios.get('http://localhost:8080/api/products').then(resp => {
     for(let i=0; i<resp.data.length; i++){
         console.log(resp.data[i].name);
         let templateProduct='<li class="container">' +
-'<img class="produto" src='+resp.data[i].photo+'>'+
-'<h3 class="descricao"><a>'+resp.data[i].name+'</a></h3>'+
-'<p class="preco">'+'R$'+resp.data[i].price+ '</p>'+
-'</li>';
+        '<img class="produto" src=' +'http://localhost:8080' +resp.data[i].photo+'>'+
+        '<h3 class="descricao"><a>'+resp.data[i].name+'</a></h3>'+
+        '<p class="preco">'+'R$'+resp.data[i].price+ '</p>'+
+        '</li>';
         prodFlex.innerHTML+=templateProduct;
 
     }
