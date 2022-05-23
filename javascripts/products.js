b7d02b2cf832961ac787b5e659121107c24c1e45
Juliana Vaz da Silva <vazjuliana1993@gmail.com>
2022-05-23 16:46:25 -0300

    usando componente produto dentro da chamada do axios

--- javascripts/products.js
+++ javascripts/products.js
@@ -7,13 +7,15 @@ axios.get('http://localhost:8080/api/products').then(resp => {
     console.log(resp.data);
 
     for(let i=0; i<resp.data.length; i++){
-        console.log(resp.data[i].name);
-        let templateProduct='<li class="container">' +
-        '<img class="produto" src=' +'http://localhost:8080' +resp.data[i].photo+'>'+
-        '<h3 class="descricao"><a>'+resp.data[i].name+'</a></h3>'+
-        '<p class="preco">'+'R$'+resp.data[i].price+ '</p>'+
-        '</li>';
-        prodFlex.innerHTML+=templateProduct;
+        /*const componentProduto = document.createElement('component-produto')
+        componentProduto.setAttribute('imagemProduto', 'http://localhost:8080'+ resp.data[i].photo)
+        componentProduto.setAttribute('nomeProduto', resp.data[i].name)
+        componentProduto.setAttribute('procoProduto', resp.data[i].price)
+            prodFlex.appendChild(componentProduto);
+        console.log(componentProduto)*/
 
+        console.log(resp.data[i].name);
+        let templateProduct= '<component-produto imagemProduto="http://localhost:8080'+ resp.data[i].photo +'" nomeProduto="'+ resp.data[i].name +'" precoProduto= "'+ resp.data[i].price +'" ></component-produto>';
+        prodFlex.innerHTML+=templateProduct
     }
 });
